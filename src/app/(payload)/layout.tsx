c36d8b85f0be70c159fb491008623dbc980fc957
Vladimir <107486166+VladimirDegt@users.noreply.github.com>
2025-01-30 12:58:37 +0200

    fix: build

--- src/app/(payload)/layout.tsx
+++ src/app/(payload)/layout.tsx
@@ -5,7 +5,6 @@ import '@payloadcms/next/css'
 import type { ServerFunctionClient } from 'payload'
 import { handleServerFunctions, RootLayout } from '@payloadcms/next/layouts'
 import React from 'react'
-import type { Metadata } from "next";
 
 import { importMap } from './admin/importMap.js'
 import './custom.scss'
