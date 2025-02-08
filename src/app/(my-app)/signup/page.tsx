89ddd919363a9fbb0ff3d7f2204cc650a4e5144f
Vladimir <107486166+VladimirDegt@users.noreply.github.com>
2025-02-08 10:27:25 +0200

    feat: signup

---
+++ src/app/(my-app)/signup/page.tsx
@@ -0,0 +1,12 @@
+import { ReactElement } from "react";
+
+import cls from "./page.module.scss";
+import SignupForm from "@/app/(my-app)/signup/components/SignupForm";
+
+export default function page(): ReactElement {
+  return (
+    <main className={cls.container}>
+      <SignupForm/>
+    </main>
+  );
+}
